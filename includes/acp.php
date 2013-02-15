a2fb30cc43bd064b6c03b7e3a696bba6532fa205
apat1k <vk@korden.ru>
2013-02-15 16:17:13 +0300

    Добавил сортировку вкладок

--- includes/acp.php
+++ includes/acp.php
@@ -69,7 +69,8 @@
 	$sql='SELECT a.tab, COUNT(a.id) AS cnt 
 		FROM '.SQL_PREFIX.'modules AS a  
 		WHERE a.parent != 0 AND a.tab IN ('.implode(',', array_keys($tabs)).')'.$qWhere.'
-		GROUP BY a.tab';
+		GROUP BY a.tab 
+		ORDER BY a.sort ASC';
 	$result=$app['db']->query($sql);
 	$arr = array();
 	while($row=$app['db']->fetchrow($result))
